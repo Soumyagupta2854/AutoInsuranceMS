7bf6e6928cbe79410f059fbc4844c3cc9e4e66d8
krishnana388 <Krishna.Narayan@cognizant.com>
2025-06-07 13:35:33 +0530

    Initial Commit

--- AutoInsuranceManagementSystem/obj/Debug/net8.0/AutoInsuranceManagementSystem.AssemblyInfo.cs
+++ AutoInsuranceManagementSystem/obj/Debug/net8.0/AutoInsuranceManagementSystem.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoInsuranceManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4aae4db634c40661e11af71ee6ff0102f07c177")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49b38e623aa98f9c0f27867eb721372f5774d544")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoInsuranceManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoInsuranceManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
